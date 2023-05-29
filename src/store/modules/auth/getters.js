f0108f59118c74f4a643ed425b6b4d3c11796ecc
Abhinand K <abhinandkofficial01@gmail.com>
2023-05-29 16:42:19 +0530

    passed token and succefully called the json data

--- src/store/modules/auth/getters.js
+++ src/store/modules/auth/getters.js
@@ -1,1 +1,7 @@
-export default {}
+import { GET_USER_TOKEN_GETTER } from '../../storeConstants'
+
+export default {
+    [GET_USER_TOKEN_GETTER]: (state) => {
+        return state.token;
+    }
+}
--- src/views/VuePost.vue
+++ src/views/VuePost.vue
@@ -6,14 +6,23 @@
 
 <script>
 import axios from 'axios'
+import { mapGetters } from 'vuex';
+import { GET_USER_TOKEN_GETTER } from '../store/storeConstants'
 
 export default {
     name: 'PostPage',
     data() {
         return
     },
+
+    computed: {
+        ...mapGetters('auth', {
+            token: GET_USER_TOKEN_GETTER,
+        })
+    },  
+
     mounted() {
-        axios.get(`https://vue-authentication-b4245-default-rtdb.firebaseio.com/tasks.json`)
+        axios.get(`https://vue-authentication-b4245-default-rtdb.firebaseio.com/tasks.json?auth=${this.token}`)
         .then((response) => {
             console.log(response);
         });
