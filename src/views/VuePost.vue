2bfac562f73df893a83d26f00c59f48e37ea9221
Abhinand K <abhinandkofficial01@gmail.com>
2023-05-29 16:32:36 +0530

    permission is denied! Reason - not passing token

--- src/views/VuePost.vue
+++ src/views/VuePost.vue
@@ -5,8 +5,19 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     name: 'PostPage',
+    data() {
+        return
+    },
+    mounted() {
+        axios.get(`https://vue-authentication-b4245-default-rtdb.firebaseio.com/tasks.json`)
+        .then((response) => {
+            console.log(response);
+        });
+    }
 }
 </script>
 
