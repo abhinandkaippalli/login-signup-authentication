ffde22408ab009404021cf23ed7e1d476dbdbbef
Abhinand K <abhinandkofficial01@gmail.com>
2023-05-30 12:08:10 +0530

    populated data to the table

--- src/views/VuePost.vue
+++ src/views/VuePost.vue
@@ -1,31 +1,66 @@
 <template>
     <div>
-        <p>Post Page</p>
+        <h3>Posts Details</h3>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Title</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="post in posts" :key="post.id">
+                    <td>{{ post.id }}</td>
+                    <td>{{ post.title }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
+
 <script>
 import axios from 'axios'
-import { mapGetters } from 'vuex';
-import { GET_USER_TOKEN_GETTER } from '../store/storeConstants'
+import { mapGetters, mapMutations } from 'vuex';
+import { GET_USER_TOKEN_GETTER, LOADING_SPINNER_SHOW_MUTATION } from '../store/storeConstants'
 
 export default {
     name: 'PostPage',
     data() {
-        return
+        return {
+            posts: []
+        }
     },
 
     computed: {
         ...mapGetters('auth', {
             token: GET_USER_TOKEN_GETTER,
         })
-    },  
+    },
 
     mounted() {
+        this.showLoading(true);
+
         axios.get(`https://vue-authentication-b4245-default-rtdb.firebaseio.com/tasks.json?auth=${this.token}`)
-        .then((response) => {
-            console.log(response);
-        });
+            .then((response) => {
+                this.formatPosts(response.data);
+                this.showLoading(false);
+            }).catch(() => {
+                this.showLoading(false)
+            })
+    },
+
+    methods: {
+        ...mapMutations({
+            showLoading: LOADING_SPINNER_SHOW_MUTATION
+        }),
+
+        formatPosts(posts) {
+            for (let key in posts) {
+                this.posts.push({ ...posts[key], id: key })
+            }
+            console.log(this.posts);
+        }
     }
 }
 </script>
