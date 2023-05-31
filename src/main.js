c75e34bf3e55ba97eac96a92a0dc9406076dfa0e
Abhinand K <abhinandkofficial01@gmail.com>
2023-05-31 10:32:40 +0530

    axios interceptor added

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/store';
+import axios from 'axios';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 
 const app = createApp(App)
 
+axios.interceptors.request.use((config) => {
+    console.log(config); 
+});
 app.use(router)
 app.use(store)
 app.mount('#app')
