511ff387b363c184c08d4c04749c73246122afa0
Abhinand K <abhinandkofficial01@gmail.com>
2023-05-31 15:23:44 +0530

    removed axios interceptor from main.js for applying it on separately

--- src/main.js
+++ src/main.js
@@ -2,20 +2,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/store';
-import axios from 'axios';
+// import axios from 'axios';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 
 const app = createApp(App)
 
-axios.interceptors.request.use((config) => {
-    // console.log(config); 
-    let params = new URLSearchParams();
-    params.append('hai', 'Abhi');
-    config.params = params;
-    return config;
-});
+// axios.interceptors.request.use((config) => {
+//     // console.log(config); 
+//     let params = new URLSearchParams();
+//     params.append('hai', 'Abhi');
+//     config.params = params;
+//     return config;
+// });
 app.use(router)
 app.use(store)
 app.mount('#app')
