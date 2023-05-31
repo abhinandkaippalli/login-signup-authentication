bbc67c0811224ae64b85f814b179bd7db62d8fe0
Abhinand K <abhinandkofficial01@gmail.com>
2023-05-31 16:43:51 +0530

    Home page created

---
+++ src/views/VueHome.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>Home Page</div>
+    <div>
+        <router-link to="/posts">Go to posts page</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'HomePage',
+})
+</script>
